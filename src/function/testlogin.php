eb6a6dc0e56c60976b51560f0f2bcaff1a61488c
BrunoWithoutH <pinheirobrunoevaristo@gmail.com>
2025-08-24 00:38:42 -0300

    Corrijindo bugs no sistema de login e registro

--- src/function/testlogin.php
+++ src/function/testlogin.php
@@ -2,36 +2,28 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-session_start();
+include("pg_config.php");
 
 if (isset($_POST['submit']) && !empty($_POST['UEmail']) && !empty($_POST['USenha'])) {
-    include_once("pg_config.php");
 
     $EmailUsuario = $_POST["UEmail"];
     $SenhaUsuario = $_POST["USenha"];
 
-    // Buscar usuário pelo email
     $sql = "SELECT * FROM usuarios WHERE emailusuario = :email LIMIT 1";
     $stmt = $pdo->prepare($sql);
     $stmt->bindParam(':email', $EmailUsuario);
     $stmt->execute();
-
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    // Verifica se usuário existe e se a senha está correta
     if ($user && password_verify($SenhaUsuario, $user['senhausuario'])) {
         $_SESSION['EmailUsuario'] = $user['emailusuario'];
         $_SESSION['NomeUsuario'] = $user['nomeusuario'];
         $_SESSION['IdUsuario'] = $user['idusuario'];
 
-        // Redirecionar para a página anterior ou home
         $redirectUrl = $_SESSION['previous_page'] ?? '/StrideBR/home.php';
         unset($_SESSION['previous_page']);
         header('Location: ' . $redirectUrl);
         exit();
-
-
     } else {
         echo "Credenciais inválidas.";
     }
