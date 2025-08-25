9840543ad19fdf7ada02fea4eeb2f1daa2da8d3d
BrunoWithoutH <pinheirobrunoevaristo@gmail.com>
2025-08-25 15:49:38 -0300

    Adiciona rodapé com direitos autorais e corrige redirecionamento de página

--- public/assets/js/atividades.js
+++ public/assets/js/atividades.js
@@ -26,18 +26,6 @@ function calcula() {
     return false;
 }
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const currentYear = new Date().getFullYear();
-    const creationYear = 2024;
-    const footer = document.querySelector('.footer p');
-
-    if (currentYear === creationYear) {
-        footer.textContent = `Feito Por Bruno Evaristo Pinheiro - ${creationYear}`;
-    } else {
-        footer.textContent = `Feito Por Bruno Evaristo Pinheiro - ${creationYear} - ${currentYear}`;
-    }
-});
-
 //mostrar formulário
 document.addEventListener('DOMContentLoaded', function () {
     const addButton = document.querySelector('.addbutton');
