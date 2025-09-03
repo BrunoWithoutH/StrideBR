b17830b1e4b3360c3812ea1e8cbe6844add80cb2
BrunoWithoutH <pinheirobrunoevaristo@gmail.com>
2025-09-02 21:35:16 -0300

    Padroniza includes pra requice_once absolutos

--- public/user/atividades.php
+++ public/user/atividades.php
@@ -3,11 +3,9 @@ date_default_timezone_set('America/Sao_Paulo');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
-require_once("../../src/config/pg_config.php");
-require_once __DIR__ . "/../../vendor/autoload.php"; 
+require_once dirname(__DIR__, 2) . '/src/config/pg_config.php';
+require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 use Hidehalo\Nanoid\Client;
-
 session_start();
 
 if (isset($_SESSION['EmailUsuario']) && isset($_SESSION['SenhaUsuario'])) {
