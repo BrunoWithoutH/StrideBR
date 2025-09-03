f5668bce9ee3f7bdb68cb4739f989c54d4993daf
BrunoWithoutH <pinheirobrunoevaristo@gmail.com>
2025-09-02 21:03:27 -0300

    Correção

--- public/user/atividades.php
+++ public/user/atividades.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once("../../src/config/pg_config.php");
+include('../../src/config/pg_config.php');
 require_once __DIR__ . "/../../vendor/autoload.php"; 
 use Hidehalo\Nanoid\Client;
 
